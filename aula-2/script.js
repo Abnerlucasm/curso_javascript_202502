59aafb5b75afbd296df7d183057e0f98c409895c
alan-rohling <alanjoaquimrohling@gmail.com>
2025-08-22 21:12:25 -0300

    Exercicio

---
+++ aula-2/script.js
@@ -0,0 +1,8 @@
+MudaCor();
+
+function MudaCor(){
+    const elemento = document.getElementById('titulo')
+    elemento.style.color = 'red'
+    const input = document.getElementById('texto')
+    console.log(input.value);
+}
