6d77ee80bc6bc2988d0e3fe5879879cccee8da4d
Carlos souza <carlosszalbuquerque@gmail.com>
2025-08-22 21:31:01 -0300

    resultadoa-atividade

--- aula-2/script.js
+++ aula-2/script.js
@@ -1,7 +1,12 @@
-MudarCor()
 
-function MudarCor(){
-    const elemento = document.getElementById('Titulo')
+
+function MudaCor(){
+    const elemento = document.getElementById('titulo')
+
     elemento.style.color = 'red'
     
+    const input = document.getElementById('texto')
+    console.log("Meu nome é: ",input.value);
+    elemento.innerText = `Meu nome é: ${input}`;
+
 }
