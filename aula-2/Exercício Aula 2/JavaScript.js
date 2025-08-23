74ddf1417fc8cfa600ef7e15d041935b522bbec5
Lara <aluno.unibave@ad.unibave.net>
2025-08-22 21:27:11 -0300

    Resolução atv 1

---
+++ aula-2/Exercício Aula 2/JavaScript.js
@@ -0,0 +1,8 @@
+
+function somar(){
+    const num1=document.getElementById('identificador').value;
+    const num2=document.getElementById('texto').value;
+    const soma =parseFloat(num1) + parseFloat(num2);
+    console.log(soma);
+    
+}
