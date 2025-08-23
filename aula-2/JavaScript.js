947f7ac8e6d1403faa6642a988d1eb25bda7f4ad
unknown <laragregorioschlickmann7165@gmail.com>
2025-08-22 21:27:21 -0300

    Resolução atividade2

---
+++ aula-2/JavaScript.js
@@ -0,0 +1,11 @@
+function somar(){
+    const numero =document.getElementById('texto').value;
+        const numero2 =document.getElementById('Identificador').value;
+        const soma =parseFloat(numero)+parseFloat(numero2);
+        console.log(soma);
+
+        
+
+
+
+}
