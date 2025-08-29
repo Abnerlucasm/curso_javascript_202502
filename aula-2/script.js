a1b134f841dc076f5eaf656a0c16464268c03305
Tiago <tiago212005@gmail.com>
2025-08-29 20:25:29 -0300

    script-para-alterar-nome-e-cor

--- aula-2/script.js
+++ aula-2/script.js
@@ -1,12 +1,18 @@
+let cor;
 
+function MudaCor() {
+  const elemento = document.getElementById("titulo");
 
-function MudaCor(){
-    const elemento = document.getElementById('titulo')
+  if (cor == 0) {
+    elemento.style.color = "red";
+    cor = 1;
+  } else {
+    elemento.style.color = "black";
+    cor = 0;
+  }
 
-    elemento.style.color = 'red'
-    
-    const input = document.getElementById('texto')
-    console.log("Meu nome é: ",input.value);
-    elemento.innerText = `Meu nome é: ${input}`;
-
+  const input = document.getElementById("texto");
+  console.log("Meu nome é: ", input.value);
+  console.log(cor);
+  elemento.innerText = `Meu nome é: ${input.value}`;
 }
