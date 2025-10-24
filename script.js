88d8b70f8a77ae98a86326f23278325a4387d330
FelisbinoJose <246760.jose@unibave.net>
2025-10-24 19:53:49 -0300

    fix: corrigi a funcao para mostrar o nome no console

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ function pegaNomeJogador() {
 
     const inputNome = document.getElementById('input-nome');
     const nomeJogador = inputNome.value;
-    return nomeJogador;
+
+    console.log("Nome do Jogador:", nomeJogador);
+
+    inputNome.value = "";
 }
